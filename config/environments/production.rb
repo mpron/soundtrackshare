e28e2ba0a159a66966d5d2ee94284874be5ac458
Mitch Pronschinske <mpronschinske@gmail.com>
2013-10-28 19:17:11 -0400

    fixed signup and email confirmation by adding back alerts to application\layout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Soundtrackshare::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!
@@ -66,4 +66,6 @@ Soundtrackshare::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { :host => 'soundtrackshare.herokuapp.com' }
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.perform_deliveries = true
 end
